d4ef35862bf32859acc24d2adcffe097e4f2a262
Ana Flávia <ana.flavia.r.honorio@accenture.com>
2022-10-05 09:02:47 -0300

    Fazendo o setup das consultas ao webservice do projeto

---
+++ app/src/main/java/com/br/anabibelforscher/filmesflix/api/MovieApi.kt
@@ -0,0 +1,11 @@
+package com.br.anabibelforscher.filmesflix.api
+
+import com.br.anabibelforscher.filmesflix.model.Movie
+import retrofit2.http.GET
+
+interface MovieApi {
+
+    @GET("anabibelforscher/FilmesFlixJson/main/moviesList")
+    fun getAllMovies(): List<Movie>
+
+}
